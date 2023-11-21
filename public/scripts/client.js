7c929c12c97a1aadf4a6b522d78bcca1f72fb26a
M-ARTHELL <makoarthell@proton.me>
2023-11-21 14:28:00 -0400

    add tweet reload after submission, errors for blank/oversized tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,31 +7,7 @@
 
 $( document ).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
+  //loading tweets
   const createTweetElement = function(tweetObj) {
     const output = `
     <article class="tweet">
@@ -46,7 +22,7 @@ $( document ).ready(function() {
     </header>
       <div class="tweet-content">${tweetObj.content.text}</div>
     <footer>
-      <div>${tweetObj.created_at}</div>
+      <div>${timeago.format(tweetObj.created_at)}</div>
       <div>
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-repeat"></i>
@@ -61,12 +37,48 @@ $( document ).ready(function() {
   const renderTweets = function(tweetArray) {
     for (const tweet of tweetArray) {
       let $tweet = createTweetElement(tweet)
-      $('.tweet-container').append($tweet);
+      $('.tweet-container').prepend($tweet);
     }
   }
   
-  renderTweets(data);
-  
+
+  //submitting tweets
+  $('.new-tweet form').on("submit", function(event) {
+    const formData = $(this).serialize();
+    event.preventDefault();
+
+    if ($('.counter').val() < 0) {
+      alert("Err: Too many characters!")
+      
+    } else if (formData === "text=") {
+      alert("Err: No text provided!")
+
+    } else {
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: formData,
+        error: (errorThrown) => console.log(errorThrown),
+        success: () => console.log('success')
+      })
+      .then(function() {
+        loadTweets()
+        $('#tweet-text').val('');
+      });
+    };
+  });
+
+
+  //load tweets
+  const loadTweets = function() {
+    $.ajax('/tweets', {
+      method: 'GET',
+      error: (err) => console.log(err),
+      success: (tweetDB) => renderTweets(tweetDB)
+    })
+  };
+
+loadTweets();
+
 });
 
 
