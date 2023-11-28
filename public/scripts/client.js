ed5914af0a56c156b7d14365aa32aa677fedb5a2
M-ARTHELL <makoarthell@proton.me>
2023-11-28 12:28:35 -0400

    add comments, fix bug where error continued to show after valid tweets, allow textarea to expand with user input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,27 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
 $( document ).ready(function() {
 
-  //loading tweets
+
+  //createTweetElement
+  //prepares tweets for display
   const createTweetElement = function(tweetObj) {
 
+    //prevents user made scripts from running
     const escape = function (str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
 
+    //adds tweet information to template
     const output = `
     <article class="tweet">
     <header>
       <div class="display-name">
-        <img src=${tweetObj.user.avatars} alt="${tweetObj.user.name}'s profile image.">
-        ${tweetObj.user.name}
+        <img src=${tweetObj.user.avatars} alt="${escape(tweetObj.user.name)}'s profile image.">
+        ${escape(tweetObj.user.name)}
       </div>
       <div class="username">
-        ${tweetObj.user.handle}
+        ${escape(tweetObj.user.handle)}
       </div>
     </header>
       <div class="tweet-content">${escape(tweetObj.content.text)}</div>
@@ -37,54 +40,66 @@ $( document ).ready(function() {
       </div>
     </footer>
   </article>
-  `;  
+  `;
+
   return output;
   };
 
+
+  //renderTweets
+  //runs array of tweets through createTweetElement and applies them to the dom
   const renderTweets = function(tweetArray) {
     for (const tweet of tweetArray) {
       let $tweet = createTweetElement(tweet)
       $('.tweet-container').prepend($tweet);
     }
   }
-  
 
-  if ($('.error').is(':hidden')) {
-    console.log('hidden')
-  } else if ($('.error').is(':visible')) {
-    console.log('visible')
-  }
 
   //submitting tweets
+  //serializes new tweet data, checks for errors, and prevents redirect when posting
   $('.new-tweet form').on("submit", function(event) {
     const formData = $(this).serialize();
     event.preventDefault();
 
-    //displays errors to user
+    //showErr
+    //displays errors to the user
     const showErr = function(error) {
       const errIcon = '<i class="fa-solid fa-triangle-exclamation"></i>'
 
-      if ($('.error').is(':visible')) {
-        $('.error').slideToggle('slow', () => {
+      //if an error is visible, hides and replaces it before showing the next one
+      if ($('.error').is(':visible') && error) {
+          $('.error').slideToggle('slow', () => {
           $('.error').empty();
           $('.error').append(errIcon, error);
         })
-      } else {
+
+      //if errors are hidden, replaces any possible previous errors before display
+      } else if ($('.error').is(':hidden') && error) {
+        $('.error').empty();
         $('.error').append(errIcon, error);
-      }
-      $('.error').slideToggle('slow')
+      } 
+      
+      //toggles error visibility
+      $('.error').slideToggle()
     };
 
 
+
+    //error for too many characters
     if ($('.counter').val() < 0) {
       if (!$('.error').is(':animated')) {
-        showErr('ERROR: Tweet is beyond character limit.');
+        showErr('ERROR: Tweet exceeds character limit.');
       }
 
+    //error for blank form
     } else if (formData === "text=") {
-      showErr('ERROR: No text Provided!')
+      showErr('ERROR: No text provided.')
 
+    //if no error, hides any error messages, sends the tweet to the server, and reloads the tweet list.
     } else {
+      showErr();
+
       $.ajax('/tweets', {
         method: 'POST',
         data: formData,
@@ -100,7 +115,8 @@ $( document ).ready(function() {
   });
 
 
-  //load tweets
+  //loadTweets
+  //gets tweet array, if no error occurs, passes array to renderTweets function
   const loadTweets = function() {
     $.ajax('/tweets', {
       method: 'GET',
@@ -109,8 +125,15 @@ $( document ).ready(function() {
     })
   };
 
-loadTweets();
+  //runs loadTweets so they appear when the user loads the page.
+  loadTweets();
 
+
+  //expands textarea as user types
+  $('#tweet-text').on('input', function() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+  })
 });
 
 
